19c41c6c5d61b7ba243f22e021764cf30f6a1deb
samwx <samuelmartins.sw@gmail.com>
2020-05-04 20:39:29 -0300

    Add icons to colored boxes and ajust width

--- src/styles/ColoredBox.ts
+++ src/styles/ColoredBox.ts
@@ -1,10 +1,24 @@
 import styled from "styled-components";
 import { colors } from "./variables/colors";
 
-export const ColoredBox = styled.div<{ background: string }>`
-    background: ${props => props.background};
+export const ColoredBox = styled.div<{ background: string, icon: string }>`
+    background: ${props => props.background || colors.lightGray};
+    ${props => props.icon
+        ? `background-image: url(${props.icon});
+           background-position: 105% 120%;
+           background-repeat: no-repeat;
+           background-size: 160px;`
+        : ''}
     color: ${colors.white};
     padding: 20px;
-    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
-    width: 100%;
+    width: 290px;
+    height: 215px;
+    font-size: 18px;
+
+    &:hover { box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25); }
+
+    h3 {
+        font-size: 24px;
+        margin-bottom: 15px;
+    }
 `;
--- src/styles/HomeBoxes.ts
+++ src/styles/HomeBoxes.ts
@@ -1,6 +1,14 @@
 import styled from 'styled-components';
 
 export const HomeBoxes = styled.div`
+    margin: 0 auto;
     margin-top: 65px;
+    margin-bottom: 65px;
     display: flex;
+    justify-content: space-between;
+    width: 70%;
+
+    a {
+        &:hover { text-decoration: none; }
+    }
 `;
