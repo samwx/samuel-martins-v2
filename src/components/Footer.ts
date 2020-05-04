676fdc97367277ed37657e7a99ffcb5d31d0c4c5
samwx <samuelmartins.sw@gmail.com>
2020-05-04 20:39:03 -0300

    Use styled components instead

---
+++ src/components/Footer.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { colors } from '../styles/variables/colors';
+
+export const Footer = styled.footer`
+    color: ${colors.lightGray};
+    font-size: 14px;
+    font-style: italic;
+    text-align: center;
+    margin-bottom: 20px;
+`;
