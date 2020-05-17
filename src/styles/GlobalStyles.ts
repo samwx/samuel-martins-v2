f19fd4afcdcdbc8e51974012740cc6fd29a3d1b0
samwx <samuelmartins.sw@gmail.com>
2020-05-17 15:06:53 -0300

    Change some vscode styles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -3,6 +3,10 @@ import { colors } from './variables/colors';
 import { fonts } from './variables/fonts';
 
 export const GlobalStyles = createGlobalStyle`
+    :root {
+        --grvsc-border-radius: 0px;
+    }
+
     body {
         background: ${colors.darkenWhite};
         color: ${colors.primaryGray};
@@ -21,5 +25,10 @@ export const GlobalStyles = createGlobalStyle`
 
     h1, h2, h3, h4, h5, h6 {
         font-family: ${fonts.family.secondary};
+    }
+
+
+    .grvsc-container {
+        margin: 0 -60px;
     }
 `;
