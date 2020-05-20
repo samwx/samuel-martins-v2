9e4a8927cad2bcafd6bd646ce7fa5c3949111987
samwx <samuelmartins.sw@gmail.com>
2020-05-19 21:42:21 -0300

    Remove inline styles...

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import { Layout } from '../components/Layout';
 
 const NotFoundPage = () => (
     <Layout>
-        <div style={{ textAlign: "center" }}>
+        <div className="tc">
             <h1>NOT FOUND</h1>
             <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
         </div>
