d685fca96b58211552ac504ed409769cb146a8b1
samwx <samuelmartins.sw@gmail.com>
2020-05-31 23:36:58 -0300

    Change post quality

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -45,7 +45,7 @@ export const BlogList: React.FunctionComponent<{}> = () => (
                                 description
                                 featuredimage {
                                     childImageSharp {
-                                        fluid(maxWidth: 120, quality: 70) {
+                                        fluid(maxWidth: 120, quality: 90) {
                                             ...GatsbyImageSharpFluid
                                         }
                                     }
